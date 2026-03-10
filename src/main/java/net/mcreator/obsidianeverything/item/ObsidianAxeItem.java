e845de889877bcd1cb2ea559b2224d28c816d72a
DOGGOSTech <doggosytmp@gmail.com>
2026-03-10 17:45:19 -0500

    Add faster axe speed

--- src/main/java/net/mcreator/obsidianeverything/item/ObsidianAxeItem.java
+++ src/main/java/net/mcreator/obsidianeverything/item/ObsidianAxeItem.java
@@ -9,7 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
 
 public class ObsidianAxeItem extends AxeItem {
-	private static final ToolMaterial TOOL_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, 100, 4f, 0, 100, TagKey.create(Registries.ITEM, ResourceLocation.parse("obsidian_everything:obsidian_axe_repair_items")));
+	private static final ToolMaterial TOOL_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, 100, 250f, 0, 100, TagKey.create(Registries.ITEM, ResourceLocation.parse("obsidian_everything:obsidian_axe_repair_items")));
 
 	public ObsidianAxeItem(Item.Properties properties) {
 		super(TOOL_MATERIAL, 19f, 11f, properties.fireResistant());
