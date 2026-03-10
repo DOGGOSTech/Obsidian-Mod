3a7bc679858c9f51f28966475ccd3aa6940da2f9
DOGGOSTech <doggosytmp@gmail.com>
2026-03-10 16:27:39 -0500

    Added Another Advancement

--- src/main/java/net/mcreator/obsidianeverything/item/ObsidianStickItem.java
+++ src/main/java/net/mcreator/obsidianeverything/item/ObsidianStickItem.java
@@ -1,9 +1,19 @@
 package net.mcreator.obsidianeverything.item;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+
+import net.mcreator.obsidianeverything.procedures.ObsidianStickItemIsCraftedsmeltedProcedure;
 
 public class ObsidianStickItem extends Item {
 	public ObsidianStickItem(Item.Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	public void onCraftedBy(ItemStack itemstack, Player entity) {
+		super.onCraftedBy(itemstack, entity);
+		ObsidianStickItemIsCraftedsmeltedProcedure.execute(entity);
 	}
 }
